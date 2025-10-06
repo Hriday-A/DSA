97339521f870b800c34475fb6575384dd18d093d
Hriday-A <142036376+Hriday-A@users.noreply.github.com>
2025-10-06 17:57:21 -0500

    Time: 18 ms (84.41%) | Memory: 16.2 MB (64.59%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        dict1={}
+        for word in strs:
+            sorted_word=''.join(sorted(word))
+            if sorted_word not in dict1:
+                dict1[sorted_word]=[]
+            dict1[sorted_word].append(word)
+
+        return list(dict1.values())
+
+
+
+
+
+
+
+
+
+
+        # for word in strs:
+        #     sorted_word=''.join(sorted(word))
+        #     if sorted_word not in dict1:
+        #         dict1[sorted_word]=[]
+        #     dict1[sorted_word].append(word)
+
+        # return list(dict1.values())
