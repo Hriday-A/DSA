60527e1ce2df5250fe1d7b70890f1ed9ec7e04d9
Hriday-A <142036376+Hriday-A@users.noreply.github.com>
2025-10-06 17:57:54 -0500

    Time: 14 ms (97.19%) | Memory: 16.3 MB (50.99%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -21,11 +21,3 @@ class Solution(object):
 
 
 
-
-        # for word in strs:
-        #     sorted_word=''.join(sorted(word))
-        #     if sorted_word not in dict1:
-        #         dict1[sorted_word]=[]
-        #     dict1[sorted_word].append(word)
-
-        # return list(dict1.values())
