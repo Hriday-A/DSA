44216353b64db7de894bc7e04b40d98635bcf918
Hriday-A <142036376+Hriday-A@users.noreply.github.com>
2026-02-27 20:19:08 -0600

    Time: 8 ms (33.68%) | Memory: 13.9 MB (78.34%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        i=0
+        while i < len(nums) - 1:
+            if nums[i]==nums[i+1]:
+                i=i+2
+            else:
+                return nums[i]
+
+        return nums[-1]
