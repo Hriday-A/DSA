6a1d386cb3f16ce612ef5ef373a4c9e91f4184f0
Hriday-A <142036376+Hriday-A@users.noreply.github.com>
2026-03-01 15:18:30 -0600

    Time: 5 ms (35.41%) | Memory: 46.8 MB (38.43%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,29 +1,19 @@
 class Solution {
     public int romanToInt(String s) {
-        HashMap<String,Integer> dic=  new HashMap<String,Integer>();
-        dic.put("I",1);
-        dic.put("V",5);
-        dic.put("X",10);
-        dic.put("L",50);
-        dic.put("C",100);
-        dic.put("D",500);
-        dic.put("M",1000);
+        HashMap<Character,Integer> dic=  new HashMap<>();
+        dic.put('I',1);
+        dic.put('V',5);
+        dic.put('X',10);
+        dic.put('L',50);
+        dic.put('C',100);
+        dic.put('D',500);
+        dic.put('M',1000);
 
         int sum=0;
-        String[] st= s.split("");
-
-        for(int i=0;i< st.length;i++){
-            if(dic.containsKey(st[i])){
-            int v= dic.get(st[i]);
-                if (i < st.length - 1 && dic.get(st[i]) < dic.get(st[i+1])) {
-                    sum-=v;
-                }
-                else{
-                    sum+=v;
-                }
-            }
+        for(int i = 0; i<s.length(); i++){
+            if(i<s.length()-1 && (dic.get(s.charAt(i+1)) > dic.get(s.charAt(i)))) sum-=dic.get(s.charAt(i));
+            else sum+= dic.get(s.charAt(i));
         }
-
         return sum;
 
 
