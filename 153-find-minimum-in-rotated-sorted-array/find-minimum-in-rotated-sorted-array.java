f2838c4292daef698852e182514ea7ef527e7151
Hriday-A <142036376+Hriday-A@users.noreply.github.com>
2026-04-11 01:48:22 -0500

    Time: 0 ms (100.00%) | Memory: 43.8 MB (62.42%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -20,3 +20,9 @@ class Solution {
         return -1;
     }
 }
+
+/*There are few things to consider to solve this question - 
+(1) The minimum is always on the other side of the sorted half - thats an observation 
+(2) Whenever you see that its a sorted array - think of Binary search (also it is  O(logn))
+(3) One mistake that i made was while submitting it was to write the following statement - realised when dry run the attempt - if (nums[low]<=nums[high]) return nums[low];
+*/ 
