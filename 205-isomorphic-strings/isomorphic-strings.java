907bd68234176c0bb28b3b736bc4767650322af7
Hriday-A <142036376+Hriday-A@users.noreply.github.com>
2026-04-14 17:02:03 -0500

    Time: 16 ms (43.03%) | Memory: 44.1 MB (31.17%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -6,15 +6,15 @@ class Solution {
         HashMap <Character,Character> smap = new HashMap<>();
         HashMap <Character,Character> tmap = new HashMap<>();
         for(int i=0;i<sl;i++){
-            char schar = s.charAt(i);
+            char schar = s.charAt(i);// each character indidvually 
             char tchar = t.charAt(i);
-            if (smap.containsKey(schar)){
-                if (tchar!=smap.get(schar)) return false;
+            if (smap.containsKey(schar)){ // check if already a value is there 
+                if (tchar!=smap.get(schar)) return false; //checks if for that schar if tchar is same or diff 
             }else{
-                smap.put(schar,tchar);
+                smap.put(schar,tchar); // puts the value if not there 
             }
             if (tmap.containsKey(tchar)){
-                if (schar!=tmap.get(tchar)) return false;
+                if (schar!=tmap.get(tchar)) return false; // same for tmap to check if vice versa is true 
             }else{
                 tmap.put(tchar,schar);
             }
