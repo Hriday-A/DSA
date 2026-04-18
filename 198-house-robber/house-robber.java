902392673f4209ca04a78a101eebe294d4fc9455
Hriday-A <142036376+Hriday-A@users.noreply.github.com>
2026-04-18 03:45:04 -0500

    Time: 0 ms (100.00%) | Memory: 42.6 MB (89.07%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -15,24 +15,39 @@
 //     }
 // }
 
-import java.util.Arrays;
+// Top Dowm approach 
+// import java.util.Arrays;
+// class Solution {
+//     int[] memo;
+//     public int rob(int[] nums) {
+//         this.memo= new int[nums.length];
+//         Arrays.fill(memo,-1);
+//         return helper(nums,0);
+//     }
+//     private int helper(int[] nums, int idx){
+//         //base
+//         if(idx>=nums.length) return 0;
+//         if(memo[idx]!=-1) return memo[idx];
+//         //logic
+//         //dont choose 
+//         int case1 = helper(nums,idx+1);
+//         //choose 
+//         int case2 = nums[idx]+helper(nums,idx+2);
+//         memo[idx]= Math.max(case1, case2);
+//         return memo[idx];
+//     }
+// }
+
 class Solution {
-    int[] memo;
     public int rob(int[] nums) {
-        this.memo= new int[nums.length];
-        Arrays.fill(memo,-1);
-        return helper(nums,0);
-    }
-    private int helper(int[] nums, int idx){
-        //base
-        if(idx>=nums.length) return 0;
-        if(memo[idx]!=-1) return memo[idx];
-        //logic
-        //dont choose 
-        int case1 = helper(nums,idx+1);
-        //choose 
-        int case2 = nums[idx]+helper(nums,idx+2);
-        memo[idx]= Math.max(case1, case2);
-        return memo[idx];
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for(int i=2; i<nums.length;i++){
+            dp[i] = Math.max(dp[i-1],nums[i]+dp[i-2]);
+        }
+        return dp[n-1];
     }
 }
