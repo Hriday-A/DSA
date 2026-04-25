48b84fff300c13dc9e3fadba20704cbff29af804
Hriday-A <142036376+Hriday-A@users.noreply.github.com>
2026-04-25 17:09:42 -0500

    Time: 0 ms (100.00%) | Memory: 43.7 MB (90.45%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,26 +1,48 @@
+// time complexity - o(m+logn) --> not most optimal 
+
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+//         int m = matrix.length;
+//         int n =matrix[0].length;
+//         int low=0;
+//         int high=n-1;
+//         for(int i=0;i<m;i++){
+//             if(matrix[i][low]<=target && matrix[i][high]>=target){
+//                 boolean flag=bs(matrix,low,high,i, target);
+//                 return flag;
+//             }
+//         }
+//         return false;
+//     }
+//     private boolean bs(int[][] matrix, int low, int high,int i, int t){
+//         while(low<=high){
+//             int mid= low+(high-low)/2;
+//             if(matrix[i][mid]== t) return true;
+//             else if(matrix[i][mid]< t){
+//                 low=mid+1;
+//             }else high= mid-1;
+//         }
+//         return false;
+//     }
+// }
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length;
-        int n =matrix[0].length;
-        int low=0;
-        int high=n-1;
-        // int i=0;
-        for(int i=0;i<m;i++){
-            if(matrix[i][low]<=target && matrix[i][high]>=target){
-                boolean flag=bs(matrix,low,high,i, target);
-                return flag;
-            }
+        int n = matrix[0].length;
+        
+        int low = 0, high = m * n - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            int row = mid / n;
+            int col = mid % n;
+            
+            if (matrix[row][col] == target) return true;
+            else if (matrix[row][col] < target) low = mid + 1;
+            else high = mid - 1;
         }
-        return false;
-    }
-    private boolean bs(int[][] matrix, int low, int high,int i, int t){
-        while(low<=high){
-            int mid= low+(high-low)/2;
-            if(matrix[i][mid]== t) return true;
-            else if(matrix[i][mid]< t){
-                low=mid+1;
-            }else high= mid-1;
-        }
+        
         return false;
     }
 }
