87e1c32486619519e72ae0ad313cc78d5bbbc8d4
Hriday-A <142036376+Hriday-A@users.noreply.github.com>
2026-05-11 02:08:21 -0500

    Time: 1 ms (100.00%) | Memory: 44.1 MB (71.46%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,3 +1,7 @@
+// time Complexity : O(m*n)
+// Space Complexity : O((m*n)/4)= O(m*n)
+// BSF Approach - 
+/*
 class Solution {
     int[][] dirs;
     int m,n;
@@ -24,7 +28,8 @@ class Solution {
 
         int time=0;
         if(fresh==0) return time;
-
+        //3. Now we will traverse for each element in Queue
+        //4. Add the ones that are not corrupted and make it corrupt and add in queue
         while(!q.isEmpty()){
             int size = q.size();
             time++;
@@ -43,5 +48,47 @@ class Solution {
             }
         }
         return -1;
+    }
+}
+*/
+class Solution {
+    int[][] dirs;
+    int m,n;
+    public int orangesRotting(int[][] grid) {
+        this.m = grid.length; 
+        this.n = grid[0].length;
+        this.dirs = new int[][]{{-1,0},{1,0},{0,1},{0,-1}};
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2){
+                    dfs(grid,i,j,2); // why are we starting from 2 ??
+                }
+            }
+        }
+        int result = 2; // i dont understand this block 
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 1) return -1;
+                else {
+                    result = Math.max(result, grid[i][j]);
+                }
+            }
+        }
+
+        return result - 2;
+    }
+    private void dfs(int[][] grid,int i, int j,int time){
+        //base
+        if(i < 0 || j < 0 || i == m || j == n) return;
+        if(grid[i][j] != 1 && grid[i][j] < time) return;
+        //logic
+        grid[i][j]=time;
+        for(int[] dir:dirs){
+            int r = dir[0]+i;
+            int c = dir[1]+j;
+            dfs(grid,r,c,time+1);
+        }
+
     }
 }
