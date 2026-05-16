1606ea2d8ab0928f417437f2defe849b3fda97d3
Hriday-A <142036376+Hriday-A@users.noreply.github.com>
2026-05-16 16:32:38 -0500

    Time: 7 ms (4.20%) | Memory: 47.2 MB (5.39%) - LeetSync

---
+++ 39-combination-sum/combination-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    List<List<Integer>> result;
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        this.result= new ArrayList<>();
+        helper(candidates,target,new ArrayList<>(),0);
+        return result;
+    }
+    private void helper(int[] candidates,int target, List<Integer> path,int i){
+        //base
+        if(target==0) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        if(target<0 || i==candidates.length) return; 
+        //logic
+        //0
+        helper(candidates,target,new ArrayList<>(path),i+1);
+        //action
+        path.add(candidates[i]);
+        // System.out.println(path);
+        //1
+        //recurse
+        helper(candidates,target-candidates[i],new ArrayList<>(path),i);
+    }
+}
