504be9ee2b10c51993bdc8a9bb4db498b323c42d
Hriday-A <142036376+Hriday-A@users.noreply.github.com>
2026-05-16 16:41:15 -0500

    Time: 2 ms (94.85%) | Memory: 45.5 MB (81.51%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,3 +1,28 @@
+// class Solution {
+//     List<List<Integer>> result;
+//     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+//         this.result= new ArrayList<>();
+//         helper(candidates,target,new ArrayList<>(),0);
+//         return result;
+//     }
+//     private void helper(int[] candidates,int target, List<Integer> path,int i){
+//         //base
+//         if(target==0) {
+//             result.add(new ArrayList<>(path));
+//             return;
+//         }
+//         if(target<0 || i==candidates.length) return; 
+//         //logic
+//         //0
+//         helper(candidates,target,new ArrayList<>(path),i+1);
+//         //action
+//         path.add(candidates[i]);
+//         // System.out.println(path);
+//         //1
+//         //recurse
+//         helper(candidates,target-candidates[i],new ArrayList<>(path),i);
+//     }
+// }
 class Solution {
     List<List<Integer>> result;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
@@ -14,12 +39,14 @@ class Solution {
         if(target<0 || i==candidates.length) return; 
         //logic
         //0
-        helper(candidates,target,new ArrayList<>(path),i+1);
+        helper(candidates,target,path,i+1);
         //action
         path.add(candidates[i]);
         // System.out.println(path);
         //1
         //recurse
-        helper(candidates,target-candidates[i],new ArrayList<>(path),i);
+        helper(candidates,target-candidates[i],path,i);
+        //backtrack
+        path.remove(path.size()-1);
     }
 }
