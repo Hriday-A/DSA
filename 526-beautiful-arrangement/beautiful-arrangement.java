73f723ab1003d8c0d215cdf8244eb24b32deabaa
Hriday-A <142036376+Hriday-A@users.noreply.github.com>
2026-05-17 13:44:07 -0500

    Time: 1798 ms (5.07%) | Memory: 46.3 MB (8.45%) - LeetSync

---
+++ 526-beautiful-arrangement/beautiful-arrangement.java
@@ -0,0 +1,29 @@
+class Solution {
+    boolean[] arr;
+    int result;
+    public int countArrangement(int n) {
+        this.arr = new boolean[n+1];
+        this.result=0;
+        helper(n,1);
+        return result;
+    }
+    public void helper(int n, int idx){
+        //base 
+        if(idx>n)
+        {
+            result++;
+            return;
+        }  
+
+        //logic 
+        for(int i=1;i<=n;i++){
+            if((arr[i]==false)&&(i%idx==0 || idx%i==0)){
+                arr[i]= true; //action 
+                helper(n,idx+1); //recurse
+                System.out.println(arr[i]);
+                //backtrack 
+                arr[i]= false;
+            }
+        }
+    }
+}
