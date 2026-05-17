45214e06bad55082a3e5b491a8be075eb00adfbe
Hriday-A <142036376+Hriday-A@users.noreply.github.com>
2026-05-16 19:33:19 -0500

    Time: 1 ms (89.01%) | Memory: 43.5 MB (99.43%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,23 +1,43 @@
+// 0-1 recursion 
+// time complexity - O(n × 2ⁿ)
+//space complexity - O(n)
+// class Solution {
+//     List<List<Integer>> result;
+//     public List<List<Integer>> subsets(int[] nums) {
+//         this.result = new ArrayList<>();
+//         List<Integer> path = new ArrayList<>();
+//         helper(nums, 0, path);
+//         return result;
+//     }
+//     private void helper(int[] nums, int i,List<Integer> path){
+//         //base
+//         if(i==nums.length)
+//         {
+//             result.add(new ArrayList<>(path));
+//             return;
+//         }
+//         //logic 
+//         //0
+//         helper(nums,i+1,path);
+//         path.add(nums[i]);
+//         helper(nums,i+1,path);
+//         path.remove(path.size()-1);
+//     }
+// }
 class Solution {
     List<List<Integer>> result;
     public List<List<Integer>> subsets(int[] nums) {
         this.result = new ArrayList<>();
         List<Integer> path = new ArrayList<>();
-        helper(nums, 0, path);
+        helper(nums,path,0);
         return result;
     }
-    private void helper(int[] nums, int i,List<Integer> path){
-        //base
-        if(i==nums.length)
-        {
-            result.add(new ArrayList<>(path));
-            return;
+    private void helper(int[] nums,List<Integer> path,int pivot){
+        result.add(new ArrayList(path));
+        for(int i=pivot;i<nums.length;i++){
+            path.add(nums[i]);
+            helper(nums,path,i+1);
+            path.remove(path.size()-1);
         }
-        //logic 
-        //0
-        helper(nums,i+1,path);
-        path.add(nums[i]);
-        helper(nums,i+1,path);
-        path.remove(path.size()-1);
     }
 }
