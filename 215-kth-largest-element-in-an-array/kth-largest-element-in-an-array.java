b1410babf6862d6402884f902cd6c46f4dbc4862
Hriday-A <142036376+Hriday-A@users.noreply.github.com>
2026-05-19 14:02:20 -0500

    Time: 51 ms (64.48%) | Memory: 71.5 MB (5.07%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,17 +1,26 @@
+//Time complexity is dependent on the size of heap and as we are just taking 
+//K size - it would be O(nlogk)
+//space complexity - O(k)
+// using min heap -
+// class Solution {
+//     public int findKthLargest(int[] nums, int k) {
+//         PriorityQueue<Integer> pq = new PriorityQueue<>(); // min heap 
+//         for(int i=0;i<nums.length;i++){
+//             pq.add(nums[i]);
+//             if(pq.size()>k) pq.poll();
+//         }
+//         return pq.peek();
+//     }
+// }
+//using max heap-
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int i:nums){
-            pq.add(i);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a); // max heap -defined by comparator 
+        int result =Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            pq.add(nums[i]);
+            if(pq.size()>(nums.length-k)) result = Math.min(result, pq.poll());
         }
-
-        for (int i=0; i<k-1; i++){
-            pq.poll();
-        }
-
-
-
-        return pq.peek();
-
+        return result;
     }
 }
