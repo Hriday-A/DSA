d4d23344ec767fb2cb3c12520678f4d34f47a4df
Hriday-A <142036376+Hriday-A@users.noreply.github.com>
2026-05-21 13:11:07 -0500

    Time: 0 ms (100.00%) | Memory: 48 MB (56.14%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double myPow(double x, int n) {
+        //base 
+        if(n==0) return 1.0;
+        //logic 
+        double res=myPow(x,n/2);
+        if(n%2==0) return res*res;
+        else{
+            if(n<0) return res*res*(1/x);
+            else return res*res*(x);
+        }
+    }
+}
