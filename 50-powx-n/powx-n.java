bdbbb81dcb8dfbaccd0f1ff2861257968e196fb9
Hriday-A <142036376+Hriday-A@users.noreply.github.com>
2026-05-21 13:26:44 -0500

    Time: 0 ms (100.00%) | Memory: 48.3 MB (21.62%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,13 +1,33 @@
+// Time Complexity = O(logn)
+// space complexity = O(logn)
+// class Solution {
+//     public double myPow(double x, int n) {
+//         //base 
+//         if(n==0) return 1.0;
+//         //logic 
+//         double res=myPow(x,n/2);
+//         if(n%2==0) return res*res;
+//         else{
+//             if(n<0) return res*res*(1/x);
+//             else return res*res*(x);
+//         }
+//     }
+// }
+
 class Solution {
     public double myPow(double x, int n) {
-        //base 
-        if(n==0) return 1.0;
-        //logic 
-        double res=myPow(x,n/2);
-        if(n%2==0) return res*res;
-        else{
-            if(n<0) return res*res*(1/x);
-            else return res*res*(x);
+        double result=1.0;
+        if (n < 0) {
+            x = 1 / x;
+            n = n * -1;
         }
+        while(n!=0){
+            if(n%2!=0){
+                result=result*x;
+            }
+            n=n/2;
+            x = x * x;
+        }
+        return result;
     }
 }
