2adc53cda852c4f4ef6f7c4616036951cfc7e84f
Hriday-A <142036376+Hriday-A@users.noreply.github.com>
2026-05-27 21:21:08 -0500

    Time: 0 ms (100.00%) | Memory: 42.8 MB (72.14%) - LeetSync

---
+++ 394-decode-string/decode-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    int i=0;
+    public String decodeString(String s) {
+        int currnum=0;
+        StringBuilder currstr = new StringBuilder();
+        // if we use strings, it creates new path everytime but stringbuilder updates the same path 
+        while(i<s.length()){
+            char c= s.charAt(i); // char at that position 
+            i++;
+            if(Character.isDigit(c)) currnum=currnum*10+c-'0';
+            // we are separating all numbers out 
+            // char -> num ( currnum =0 initally )
+            //change to currnumm =1, then 12 then 123...
+            else if(c == '['){
+                String bb = decodeString(s);
+                for(int k=0;k<currnum;k++){
+                    currstr.append(bb);
+                }
+                currnum=0;
+            }else if(c==']') return currstr.toString();
+            else currstr.append(c);
+
+        }
+        return currstr.toString();
+    }
+}
