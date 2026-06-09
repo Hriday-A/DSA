8e5a04c1c7e7763650ad3800d731ba961ed22759
Hriday-A <142036376+Hriday-A@users.noreply.github.com>
2026-06-08 22:35:54 -0500

    Time: 28 ms (11.11%) | Memory: 49.5 MB (70.31%) - LeetSync

--- 561-array-partition/array-partition.java
+++ 561-array-partition/array-partition.java
@@ -1,3 +1,6 @@
+//Time Complexity - O(nlogn)
+//Space Complexity - O(1)
+/*
 class Solution {
     public int arrayPairSum(int[] nums) {
         Arrays.sort(nums);
@@ -6,5 +9,32 @@ class Solution {
             sum+=nums[i];
         }
         return sum;
+    }
+}
+*/
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int max=0;
+        int min=0;
+        for(int i=0;i<nums.length;i++){
+            max=Math.max(max,nums[i]);
+            min = Math.min(min,nums[i]);
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        boolean flag= true;
+        int add=0;
+        for(int i=min;i<=max;i++){
+            if(map.containsKey(i)){
+                int cnt= map.get(i);
+                while(cnt-->0){
+                    if(flag){
+                        add+=i;
+                    }
+                    flag=!flag;
+                }
+            }
+        }
+        return add;
     }
 }
