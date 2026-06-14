f8b2bdbc2518bbc828037feb81d088bae77c8603
Hriday-A <142036376+Hriday-A@users.noreply.github.com>
2026-06-14 10:24:50 -0500

    Time: 0 ms (100.00%) | Memory: 43 MB (95.65%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,11 +1,30 @@
 class Solution {
     public int hIndex(int[] citations) {
-        Arrays.sort(citations);
-        int j=citations.length;
-        for(int i=0;i<citations.length;i++){
-            if(citations[i]>=j) return j;
-            j--;
+        int n=citations.length;
+        int[] bucket= new int[n+1];
+        // reduce sorting using bucket sort
+        for(int i:citations){
+            if(i>n) bucket[n]++;
+            else bucket[i]++;
+        }
+        int sum=0;
+        for(int j=n;j>0;j--){
+            sum+=bucket[j];
+            if(sum>=j) return j;
         }
         return 0;
     }
 }
+
+//nlogn 
+// class Solution {
+//     public int hIndex(int[] citations) {
+//         Arrays.sort(citations);
+//         int j=citations.length;
+//         for(int i=0;i<citations.length;i++){
+//             if(citations[i]>=j) return j;
+//             j--;
+//         }
+//         return 0;
+//     }
+// }
