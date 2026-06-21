decc33ffc269dab26f8ac386b979d68665234e8c
Hriday-A <142036376+Hriday-A@users.noreply.github.com>
2026-06-21 01:34:28 -0500

    Time: 1 ms (99.71%) | Memory: 47.6 MB (8.36%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -21,29 +21,48 @@
 //     }
 // }
 
+// class Solution {
+//     public int jump(int[] nums) {
+//         int n=nums.length;
+//         if(n==1) return 0;
+//         HashSet<Integer> set= new HashSet<>();
+//         Queue<Integer> q = new LinkedList<>();
+//         q.add(0);
+//         int level=0;
+//         while(!q.isEmpty()){
+//             int size = q.size();
+//             for(int i=0;i<size;i++){
+//                 int curr=q.poll();
+//                 for(int k=1;k<=nums[curr];k++){
+//                     int newi= curr+k;
+//                     if(newi>=n-1) return level+1;
+//                     if(!set.contains(newi)){
+//                         q.add(newi);
+//                         set.add(newi);
+//                     }
+//                 }
+//             }
+//             level++;
+//         }
+//         return level;
+//     }
+// }
+
 class Solution {
     public int jump(int[] nums) {
-        int n=nums.length;
+        int n= nums.length;
+        int coverage =0;
+        int farthest= 0;
+        int jump=0;
         if(n==1) return 0;
-        HashSet<Integer> set= new HashSet<>();
-        Queue<Integer> q = new LinkedList<>();
-        q.add(0);
-        int level=0;
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i=0;i<size;i++){
-                int curr=q.poll();
-                for(int k=1;k<=nums[curr];k++){
-                    int newi= curr+k;
-                    if(newi>=n-1) return level+1;
-                    if(!set.contains(newi)){
-                        q.add(newi);
-                        set.add(newi);
-                    }
-                }
+        for(int i=0;i<n-1;i++){
+            coverage = Math.max(coverage,i+nums[i]);
+            if(i==farthest){
+                farthest = coverage;
+                jump++;
+                if(coverage>=n-1) return jump;
             }
-            level++;
         }
-        return level;
+        return jump;
     }
 }
