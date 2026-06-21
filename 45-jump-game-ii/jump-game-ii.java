86d6b0b29c777505befaf8666a1992920405c52f
Hriday-A <142036376+Hriday-A@users.noreply.github.com>
2026-06-21 00:55:54 -0500

    Time: 344 ms (5.07%) | Memory: 51.9 MB (8.36%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,22 +1,49 @@
+// class Solution {
+//     HashMap<Integer,Integer> map;
+//     int n ;
+//     public int jump(int[] nums) {
+//         this.n = nums.length;
+//         map = new HashMap<>();
+//         return dfs(nums,0);
+//     }
+//     private int dfs(int[] nums, int curr){
+//         //base 
+//         if(curr>=n-1) return 0;
+//         if(map.containsKey(curr)) return map.get(curr);
+//         //logic 
+//         int min=99999;
+//         for(int i=1;i<=nums[curr];i++){
+//             int newI= curr+i;
+//             min= Math.min(min,dfs(nums,newI)+1);
+//         }
+//         map.put(curr,min);
+//         return min;
+//     }
+// }
+
 class Solution {
-    HashMap<Integer,Integer> map;
-    int n ;
     public int jump(int[] nums) {
-        this.n = nums.length;
-        map = new HashMap<>();
-        return dfs(nums,0);
-    }
-    private int dfs(int[] nums, int curr){
-        //base 
-        if(curr>=n-1) return 0;
-        if(map.containsKey(curr)) return map.get(curr);
-        //logic 
-        int min=99999;
-        for(int i=1;i<=nums[curr];i++){
-            int newI= curr+i;
-            min= Math.min(min,dfs(nums,newI)+1);
+        int n=nums.length;
+        if(n==1) return 0;
+        HashSet<Integer> set= new HashSet<>();
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        int level=0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                int curr=q.poll();
+                for(int k=1;k<=nums[curr];k++){
+                    int newi= curr+k;
+                    if(newi>=n-1) return level+1;
+                    if(!set.contains(newi)){
+                        q.add(newi);
+                        set.add(newi);
+                    }
+                }
+            }
+            level++;
         }
-        map.put(curr,min);
-        return min;
+        return level;
     }
 }
