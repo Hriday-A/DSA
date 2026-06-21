3558fb2b23c5834cb0893cab6d4223d36309da9c
Hriday-A <142036376+Hriday-A@users.noreply.github.com>
2026-06-20 19:20:14 -0500

    Time: 1313 ms (5.01%) | Memory: 50.9 MB (8.96%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,12 +1,35 @@
+// class Solution {
+//     public boolean canJump(int[] nums) {
+//         int n= nums.length;
+//         int t= n-1;
+//         for(int i=n-2;i>=0;i--){
+//             if(i+nums[i]>=t){
+//                 t=i;
+//             }
+//         }
+//         return t==0;
+//     }
+// }
+
+// DFS 
 class Solution {
+    int n;
+    HashSet<Integer> set;
     public boolean canJump(int[] nums) {
-        int n= nums.length;
-        int t= n-1;
-        for(int i=n-2;i>=0;i--){
-            if(i+nums[i]>=t){
-                t=i;
-            }
+        this.n=nums.length;
+        this.set = new HashSet<>();
+        return dfs(nums,0);
+    }
+    private boolean dfs(int[] nums,int currI){
+        //base case
+        if(currI==n-1) return true;
+        if(set.contains(currI)) return false;
+        //logic 
+        for(int k=1;k<=nums[currI];k++){
+            int newI = currI+k;
+            if(dfs(nums,newI)) return true;
         }
-        return t==0;
+        set.add(currI);
+        return false;
     }
 }
