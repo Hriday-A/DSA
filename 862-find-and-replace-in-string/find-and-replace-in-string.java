bf2b326db67ac5aca60fcae66c7b91b22f2a404f
Hriday-A <142036376+Hriday-A@users.noreply.github.com>
2026-07-26 11:13:24 -0500

    Time: 3 ms (44.25%) | Memory: 43.8 MB (45.27%) - LeetSync

--- 862-find-and-replace-in-string/find-and-replace-in-string.java
+++ 862-find-and-replace-in-string/find-and-replace-in-string.java
@@ -2,45 +2,35 @@ class Solution {
     public String findReplaceString(String s, int[] indices, String[] sources, String[] targets) {
 
         HashMap<Integer, List<Integer>> map = new HashMap<>();
-
         for (int i = 0; i < indices.length; i++) {
-            map.computeIfAbsent(indices[i], k -> new ArrayList<>()).add(i);
+            List<Integer> list = map.getOrDefault(indices[i], new ArrayList<>());
+            list.add(i);
+            map.put(indices[i], list);
         }
-
         StringBuilder sb = new StringBuilder();
-
         int i = 0;
         int n = s.length();
 
         while (i < n) {
-
             if (map.containsKey(i)) {
-
                 boolean replaced = false;
-
                 for (int l : map.get(i)) {
-
                     int m = sources[l].length();
-
                     if (i + m <= n &&
                         s.substring(i, i + m).equals(sources[l])) {
-
                         sb.append(targets[l]);
                         i += m;
                         replaced = true;
                         break;
                     }
                 }
-
                 if (replaced) {
                     continue;
                 }
             }
-
             sb.append(s.charAt(i));
             i++;
         }
-
         return sb.toString();
     }
 }
