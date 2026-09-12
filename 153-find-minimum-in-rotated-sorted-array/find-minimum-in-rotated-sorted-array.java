d35b8f546cfbf6fcd1c25f24ec4cdd45a7479afa
Hriday-A <142036376+Hriday-A@users.noreply.github.com>
2026-09-13 03:21:23 +0400

    Time: 7 ms (1.89%) | Memory: 43.7 MB (55.35%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,28 +1,17 @@
 class Solution {
     public int findMin(int[] nums) {
-        int low =0;
-        int n = nums.length;
-        int high = n-1;
-        while(low<=high){
-            if (nums[low]<=nums[high]) return nums[low];
-            int mid = low + (high-low)/2;
-            if ((mid == 0 || nums[mid] < nums[mid - 1]) &&
-                (mid == n - 1 || nums[mid] < nums[mid + 1])) {
-                return nums[mid];
-            }
-            else if(nums[mid]>=nums[low]){
-                low = mid + 1;
+        int l=0;
+        int r=nums.length-1;
+        while(l<r){
+            int mid = l+(r-l)/2;
+            if(nums[r]>nums[mid]){
+                r=mid;
+                System.out.println("r="+r);
             }else{
-                high = mid -1 ;
+                l=mid+1;
+                System.out.println("l="+l);
             }
         }
-
-        return -1;
+        return nums[l];
     }
 }
-
-/*There are few things to consider to solve this question - 
-(1) The minimum is always on the other side of the sorted half - thats an observation 
-(2) Whenever you see that its a sorted array - think of Binary search (also it is  O(logn))
-(3) One mistake that i made was while submitting it was to write the following statement - realised when dry run the attempt - if (nums[low]<=nums[high]) return nums[low];
-*/ 
