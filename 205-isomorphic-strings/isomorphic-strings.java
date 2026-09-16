b977bb1b4d44833dc90bb9551ace28e105a4cb23
Hriday-A <142036376+Hriday-A@users.noreply.github.com>
2026-09-16 08:36:10 +0400

    Time: 14 ms (50.17%) | Memory: 43.8 MB (74.03%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,25 +1,20 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        int sl= s.length();
-        int tl=t.length();
-        if (sl!=tl) return false;
-        HashMap <Character,Character> smap = new HashMap<>();
-        HashMap <Character,Character> tmap = new HashMap<>();
-        for(int i=0;i<sl;i++){
-            char schar = s.charAt(i);// each character indidvually 
-            char tchar = t.charAt(i);
-            if (smap.containsKey(schar)){ // check if already a value is there 
-                if (tchar!=smap.get(schar)) return false; //checks if for that schar if tchar is same or diff 
-            }else{
-                smap.put(schar,tchar); // puts the value if not there 
+        char[] sc=s.toCharArray();
+        char[] tc=t.toCharArray();
+        HashMap<Character,Character> map = new HashMap<>();
+        HashMap<Character,Character> map2 = new HashMap<>();
+        if(s.length()!=t.length()) return false;
+        for(int i=0;i<sc.length;i++){
+            if(!map.containsKey(sc[i])){
+                map.put(sc[i],tc[i]);
             }
-            if (tmap.containsKey(tchar)){
-                if (schar!=tmap.get(tchar)) return false; // same for tmap to check if vice versa is true 
-            }else{
-                tmap.put(tchar,schar);
+            if(map.get(sc[i])!=tc[i]) return false;
+            if(!map2.containsKey(tc[i])){
+                map2.put(tc[i],sc[i]);
             }
+            if(map2.get(tc[i])!=sc[i]) return false;
         }
         return true;
-
     }
 }
