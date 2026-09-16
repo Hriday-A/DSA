9bcb0ce56d5d25142adc818c07365d630f8c2810
Hriday-A <142036376+Hriday-A@users.noreply.github.com>
2026-09-16 08:12:34 +0400

    Time: 6 ms (98.99%) | Memory: 49.3 MB (95.90%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,17 +1,15 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String, List<String>> map = new HashMap<>();
-        for(String str:strs){ // --> str='eat','ate',.....
-            char[] charArr = str.toCharArray();//['e','a','t']
-            Arrays.sort(charArr);// ['a','e','t']
-            String sorted = String.valueOf(charArr); // sorted = 'aet'
-            if (!map.containsKey(sorted)){ // checking if sorted (aet) already exists or not in the hashmap
-                map.put(sorted, new ArrayList<>()); // it doesnt, we add 
-            } // but if it does 
-            List<String> li = map.get(sorted); // we first call the list to the corresponding key to add the value in
-            li.add(str); // we add the value in the list
-            map.put(sorted,li); // we updated the HashMap in we new values 
+        for(String s:strs){
+            char[] c=s.toCharArray();
+            Arrays.sort(c);
+            String sorted= new String(c);
+            if(!map.containsKey(sorted)){
+                map.put(sorted,new ArrayList<>());
+            }
+            map.get(sorted).add(s);
         }
-        return new ArrayList<>(map.values()); // we create a new Arraylist that is combination of all the values of our map
+        return new ArrayList<>(map.values());
     }
 }
